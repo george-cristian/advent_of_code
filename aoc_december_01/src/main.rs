fe51e2e2257bf4ae221878ca2815384be2adf4af
George Cristian <george.crist5@gmail.com>
2022-01-24 23:03:12 +0100

    Implementation for the first part of december 1st

--- aoc_december_01/src/main.rs
+++ aoc_december_01/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+
+    let input_file = File::open("./src/inputs/input_aoc_december_01.txt").unwrap();
+    let input_reader = BufReader::new(input_file);
+
+    let mut previous_measurement:i32 = -1;
+    let mut nr_of_increases = 0;
+
+    for (_index, line) in input_reader.lines().enumerate() {
+
+        let line_string = line.unwrap();
+
+        let current_measurement = line_string.to_string().parse::<i32>().unwrap();
+
+        if previous_measurement != -1 && current_measurement > previous_measurement {
+            nr_of_increases += 1;
+        }
+
+        previous_measurement = current_measurement;
+    }
+
+    println!("Total number of increases: {}", nr_of_increases);
+
 }
